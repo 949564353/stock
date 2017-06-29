8e600afd554d5a55a5f6396040e376eece403a91
freeman <949564353@qq.com>
2017-06-29 17:34:30 +0800

    add timed task

--- src/main/java/com/zrf/stock/util/HttpClientUtil.java
+++ src/main/java/com/zrf/stock/util/HttpClientUtil.java
@@ -27,24 +27,48 @@ import com.zrf.stock.entity.CqsscData;
 import com.zrf.stock.service.CqsscServiceI;
 
 
+@SuppressWarnings("deprecation")
 @Component
 public class HttpClientUtil {
 	
 	@Resource
 	private CqsscServiceI service;
 	
-	//http://t.apiplus.cn/newly.do?token=bf56ef2ad222836615d84281d5f9bd5f&code=cqssc&format=json
-	public static String CQSSC_URL = "http://a.apiplus.net/newly.do?token=2a2d75c8c792176f&code=cqssc&format=json";
-	public static String CQSSC_URL_DAY = "http://a.apiplus.net/daily.do?token=2a2d75c8c792176f&code=cqssc&format=json";	//&date=2017-05-14
+	public static String SSQ_TYPE_CQSSC = "cqssc";		//重庆时时彩
+	public static String SSQ_TYPE_TJSSC = "tjssc";		//天津时时彩
+	public static String SSQ_TYPE_XJSSC = "xjssc";		//新疆时时彩
 	
+	//时时彩当前开奖接口和日期开奖接口
+	public static String SSC_URL = "http://a.apiplus.net/newly.do?token=2a2d75c8c792176f&format=json";	//&code=cqssc
+	public static String SSC_URL_DAY = "http://a.apiplus.net/daily.do?token=2a2d75c8c792176f&format=json";	//&code=cqssc
+	
+	//插入开奖号码的开始与结束时时
 	public static String beginDay = "2017-06-18";
 	public static String endDay = "2017-06-19";
 	
 	
-	//@Scheduled(cron = "0 0/5 * * * ?")  
-	public void insertData(String url) throws ParseException, IOException{
+	
+	@Scheduled(cron = "0 2/10 10-22 * * ?")  
+	public void execute() throws ClientProtocolException, IOException{
+		insertData("cqssc",SSC_URL);		//重庆时时彩
+	}
+	
+	@Scheduled(cron = "0 2/5 0,1,22,23 * * ?")  
+	public void execute2() throws ParseException, IOException{
+		insertData("cqssc",SSC_URL);		//重庆时时彩
+	}
+	
+	@Scheduled(cron = "0 55 18 * * ?")   
+	public void executeDays() throws ParseException, IOException, InterruptedException{
+		executeDayData("cqssc",HttpClientUtil.beginDay,HttpClientUtil.endDay);		//重庆时时彩
+	}
+	
+	
+	@SuppressWarnings({ "resource" })
+	public void insertData(String sscType,String url) throws ParseException, IOException{
+		String realUrl = url+"&code="+sscType;
 		DefaultHttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
+        HttpGet request = new HttpGet(realUrl);
         HttpResponse response = client.execute(request);
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             /**读取服务器返回过来的json字符串数据**/
@@ -58,40 +82,167 @@ public class HttpClientUtil {
             SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
             String now = sdf.format(new Date()); 
             System.out.println("===========================current="+now);
-            //System.out.print("======");
-            for(int i=0;i<dataArray.size();i++){
-            	JsonObject object = dataArray.get(i).getAsJsonObject();
-            	System.out.println("&&&&&&&&&&&&&"+object.get("expect").getAsString());
-            	CqsscData temp = service.selectByPrimaryKey(object.get("expect").getAsString());
-            	if(temp==null){
-            		CqsscData cell = new CqsscData();
-                	cell.setID(object.get("expect").getAsString());
-                	cell.setDAY(object.get("expect").getAsString());
-                	String opencode = object.get("opencode").getAsString();
-                	cell.setNum(opencode.replace(",", ""));
-                	cell.setNumW(Integer.valueOf(opencode.split(",")[0]));
-                	cell.setNumQ(Integer.valueOf(opencode.split(",")[1]));
-                	cell.setNumB(Integer.valueOf(opencode.split(",")[2]));
-                	cell.setNumS(Integer.valueOf(opencode.split(",")[3]));
-                	cell.setNumG(Integer.valueOf(opencode.split(",")[4]));
-                	fillOtherColumn(cell);
-                	service.save(cell);
-            	}else{
-            		System.out.println(object.get("expect").getAsString()+":"+object.get("opencode").getAsString()+";");
-            	}
+            if(SSQ_TYPE_CQSSC.equals(sscType)){		//重庆时时彩
+            	insertCqsscCell(dataArray);
+            }else if(SSQ_TYPE_TJSSC.equals(sscType)){	//天津时时彩 
+            	insertTjsscCell(dataArray);
+            }else if(SSQ_TYPE_XJSSC.equals(sscType)){	//新疆时时彩
+            	insertXjsscCell(dataArray);
             }
+            
         } else {
             System.out.println("未得到数据！");
         }
 	}
+
+
+	/**
+	 * 插入重庆时时彩数据 
+	 * @param dataArray
+	 */
+	private void insertCqsscCell(JsonArray dataArray) {
+		for(int i=0;i<dataArray.size();i++){
+			JsonObject object = dataArray.get(i).getAsJsonObject();
+			System.out.println("&&&&&&&&&&&&&"+object.get("expect").getAsString());
+			CqsscData temp = service.selectByPrimaryKey(object.get("expect").getAsString());
+			if(temp==null){
+				CqsscData cell = new CqsscData();
+		    	cell.setID(object.get("expect").getAsString());
+		    	cell.setDAY(object.get("expect").getAsString());
+		    	String opencode = object.get("opencode").getAsString();
+		    	cell.setNum(opencode.replace(",", ""));
+		    	cell.setNumW(Integer.valueOf(opencode.split(",")[0]));
+		    	cell.setNumQ(Integer.valueOf(opencode.split(",")[1]));
+		    	cell.setNumB(Integer.valueOf(opencode.split(",")[2]));
+		    	cell.setNumS(Integer.valueOf(opencode.split(",")[3]));
+		    	cell.setNumG(Integer.valueOf(opencode.split(",")[4]));
+		    	fillCqsscOtherColumn(cell);
+		    	service.save(cell);
+			}else{
+				System.out.println(object.get("expect").getAsString()+":"+object.get("opencode").getAsString()+";");
+			}
+		}
+	}
 	
 	
 	/**
-	 * 填充10路三星开奖号码类型
+	 * 插入新疆时时彩数据 
+	 * @param dataArray
+	 */
+	private void insertXjsscCell(JsonArray dataArray) {
+		for(int i=0;i<dataArray.size();i++){
+			JsonObject object = dataArray.get(i).getAsJsonObject();
+			System.out.println("&&&&&&&&&&&&&"+object.get("expect").getAsString());
+			CqsscData temp = service.selectByPrimaryKey(object.get("expect").getAsString());
+			if(temp==null){
+				CqsscData cell = new CqsscData();
+		    	cell.setID(object.get("expect").getAsString());
+		    	cell.setDAY(object.get("expect").getAsString());
+		    	String opencode = object.get("opencode").getAsString();
+		    	cell.setNum(opencode.replace(",", ""));
+		    	cell.setNumW(Integer.valueOf(opencode.split(",")[0]));
+		    	cell.setNumQ(Integer.valueOf(opencode.split(",")[1]));
+		    	cell.setNumB(Integer.valueOf(opencode.split(",")[2]));
+		    	cell.setNumS(Integer.valueOf(opencode.split(",")[3]));
+		    	cell.setNumG(Integer.valueOf(opencode.split(",")[4]));
+		    	fillCqsscOtherColumn(cell);
+		    	service.save(cell);
+			}else{
+				System.out.println(object.get("expect").getAsString()+":"+object.get("opencode").getAsString()+";");
+			}
+		}
+	}
+	
+	
+	/**
+	 * 插入天津时时彩数据 
+	 * @param dataArray
+	 */
+	private void insertTjsscCell(JsonArray dataArray) {
+		for(int i=0;i<dataArray.size();i++){
+			JsonObject object = dataArray.get(i).getAsJsonObject();
+			System.out.println("&&&&&&&&&&&&&"+object.get("expect").getAsString());
+			CqsscData temp = service.selectByPrimaryKey(object.get("expect").getAsString());
+			if(temp==null){
+				CqsscData cell = new CqsscData();
+		    	cell.setID(object.get("expect").getAsString());
+		    	cell.setDAY(object.get("expect").getAsString());
+		    	String opencode = object.get("opencode").getAsString();
+		    	cell.setNum(opencode.replace(",", ""));
+		    	cell.setNumW(Integer.valueOf(opencode.split(",")[0]));
+		    	cell.setNumQ(Integer.valueOf(opencode.split(",")[1]));
+		    	cell.setNumB(Integer.valueOf(opencode.split(",")[2]));
+		    	cell.setNumS(Integer.valueOf(opencode.split(",")[3]));
+		    	cell.setNumG(Integer.valueOf(opencode.split(",")[4]));
+		    	fillCqsscOtherColumn(cell);
+		    	service.save(cell);
+			}else{
+				System.out.println(object.get("expect").getAsString()+":"+object.get("opencode").getAsString()+";");
+			}
+		}
+	}
+	
+	
+	/**
+	 * 填充10路三星开奖号码类型(重庆时时彩)
 	 * @param cell
 	 * @return
 	 */
-	public CqsscData fillOtherColumn(CqsscData cell){
+	public CqsscData fillCqsscOtherColumn(CqsscData cell){
+		Integer dataW = cell.getNumW();
+		Integer dataQ = cell.getNumQ();
+		Integer dataB = cell.getNumB();
+		Integer dataS = cell.getNumS();
+		Integer dataG = cell.getNumG();
+		
+		cell.setIsWqb(getType(dataW.intValue(),dataQ.intValue(),dataB.intValue()));   //前三
+		cell.setIsQbs(getType(dataQ.intValue(),dataB.intValue(),dataS.intValue()));   //中三
+		cell.setIsBsg(getType(dataB.intValue(),dataS.intValue(),dataG.intValue()));   //后三
+		cell.setIsWqs(getType(dataW.intValue(),dataQ.intValue(),dataS.intValue()));   //万千十
+		cell.setIsWqg(getType(dataW.intValue(),dataQ.intValue(),dataG.intValue()));   //万千个
+		cell.setIsWbs(getType(dataW.intValue(),dataB.intValue(),dataS.intValue()));   //万百十
+		cell.setIsWbg(getType(dataW.intValue(),dataB.intValue(),dataG.intValue()));   //万百个
+		cell.setIsWsg(getType(dataW.intValue(),dataS.intValue(),dataG.intValue()));   //万十个
+		cell.setIsQbg(getType(dataQ.intValue(),dataB.intValue(),dataG.intValue()));   //千百个
+		cell.setIsQsg(getType(dataQ.intValue(),dataS.intValue(),dataG.intValue()));   //千十个
+		
+		return cell;
+	}
+	
+	
+	/**
+	 * 填充10路三星开奖号码类型(天津时时彩)
+	 * @param cell
+	 * @return
+	 */
+	public CqsscData fillTjsscOtherColumn(CqsscData cell){
+		Integer dataW = cell.getNumW();
+		Integer dataQ = cell.getNumQ();
+		Integer dataB = cell.getNumB();
+		Integer dataS = cell.getNumS();
+		Integer dataG = cell.getNumG();
+		
+		cell.setIsWqb(getType(dataW.intValue(),dataQ.intValue(),dataB.intValue()));   //前三
+		cell.setIsQbs(getType(dataQ.intValue(),dataB.intValue(),dataS.intValue()));   //中三
+		cell.setIsBsg(getType(dataB.intValue(),dataS.intValue(),dataG.intValue()));   //后三
+		cell.setIsWqs(getType(dataW.intValue(),dataQ.intValue(),dataS.intValue()));   //万千十
+		cell.setIsWqg(getType(dataW.intValue(),dataQ.intValue(),dataG.intValue()));   //万千个
+		cell.setIsWbs(getType(dataW.intValue(),dataB.intValue(),dataS.intValue()));   //万百十
+		cell.setIsWbg(getType(dataW.intValue(),dataB.intValue(),dataG.intValue()));   //万百个
+		cell.setIsWsg(getType(dataW.intValue(),dataS.intValue(),dataG.intValue()));   //万十个
+		cell.setIsQbg(getType(dataQ.intValue(),dataB.intValue(),dataG.intValue()));   //千百个
+		cell.setIsQsg(getType(dataQ.intValue(),dataS.intValue(),dataG.intValue()));   //千十个
+		
+		return cell;
+	}
+	
+	
+	/**
+	 * 填充10路三星开奖号码类型(新疆时时彩)
+	 * @param cell
+	 * @return
+	 */
+	public CqsscData fillXjsscOtherColumn(CqsscData cell){
 		Integer dataW = cell.getNumW();
 		Integer dataQ = cell.getNumQ();
 		Integer dataB = cell.getNumB();
@@ -129,23 +280,9 @@ public class HttpClientUtil {
 		}
 		return  rtnInt;
 	}
+
 	
-	@Scheduled(cron = "0 2/10 10-22 * * ?")  
-	public void execute() throws ClientProtocolException, IOException{
-		insertData(CQSSC_URL);
-	}
-	
-	@Scheduled(cron = "0 2/5 0,1,22,23 * * ?")  
-	public void execute2() throws ParseException, IOException{
-		insertData(CQSSC_URL);
-	}
-	
-	@Scheduled(cron = "0 55 18 * * ?")   
-	public void executeDays() throws ParseException, IOException, InterruptedException{
-		executeDayData(HttpClientUtil.beginDay,HttpClientUtil.endDay);
-	}
-	
-	public void executeDayData(String beginDay,String endDay)throws ParseException, IOException, InterruptedException{
+	public void executeDayData(String sscType,String beginDay,String endDay)throws ParseException, IOException, InterruptedException{
 		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");  
 		DateTime beginTime = DateTime.parse(beginDay, formatter);
 		DateTime endTime = DateTime.parse(endDay, formatter);
@@ -153,9 +290,8 @@ public class HttpClientUtil {
 		String nextDay = beginTime.plusDays(1).toString(formatter);
 		do{
 			System.out.println("##########execute day:"+beginDay+" bgein");
-			String fullUrl = HttpClientUtil.CQSSC_URL_DAY+"&date="+beginDay;
-			System.out.println("^^^^^^^"+fullUrl);
-			insertData(fullUrl);
+			String fullUrl = HttpClientUtil.SSC_URL_DAY+"&date="+beginDay+"&code="+sscType;
+			insertData(sscType,fullUrl);
 			System.out.println("##########execute day:"+beginDay+" end");
 			beginDay = nextDay;
 			beginTime = beginTime.plusDays(1);
